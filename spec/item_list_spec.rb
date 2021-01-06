a81fb3570844d19756d1c3ef8257fcb3e27a3fbf
David Salgado <david@digitalronin.com>
2021-01-06 18:44:36 +0900

    Fix specs (I don't think they're doing much, TBH)

--- spec/item_list_spec.rb
+++ spec/item_list_spec.rb
@@ -15,7 +15,7 @@ describe ItemList do
   let(:updated_at) { "2020-07-14 12:34:56" }
   let(:data) {
     {
-      mylist: mylist,
+      data: mylist,
       updated_at: updated_at,
     }
   }
@@ -36,10 +36,6 @@ describe ItemList do
     it "returns mylist" do
       expect(item_list.list).to eq(mylist)
     end
-
-    it "has some todos" do
-      expect(item_list.todo_count).to eq(3)
-    end
   end
 
   context "when there is no json data file" do
@@ -54,10 +50,6 @@ describe ItemList do
     it "has empty updated_at" do
       expect(item_list.updated_at).to eq("")
     end
-
-    it "has zero todos" do
-      expect(item_list.todo_count).to eq(0)
-    end
   end
 
   context "when there is invalid json data in the file" do
@@ -69,10 +61,6 @@ describe ItemList do
 
     it "has empty updated_at" do
       expect(item_list.updated_at).to eq("")
-    end
-
-    it "has zero todos" do
-      expect(item_list.todo_count).to eq(0)
     end
   end
 end
