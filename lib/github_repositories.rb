295542649b9c4c28e089db24c461aced77b483eb
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-01-28 12:01:26 +0000

    Update github_repositories.rb
    add has_require_approvals_enabled issue and minor fixes

--- lib/github_repositories.rb
+++ lib/github_repositories.rb
@@ -18,9 +18,10 @@ class GithubRepository
       "default_branch_main" => "The default branch is not `main`",
       "has_default_branch_protection" => "Branch protection is not enabled for `#{default_branch}`",
       "requires_approving_reviews" => "Pull request reviews are not required",
-      "administrators_require_review" => "Administrator PRs do not require reviews",
+      "administrators_require_review" => "Administrator pull requests do not require reviews",
       "issues_section_enabled" => "The issues section is not enabled",
-      "requires_code_owner_reviews" => "Pull request code owner reviews are not required"
+      "requires_code_owner_reviews" => "Pull request code owner reviews are not required",
+      "has_require_approvals_enabled" => "Pull request review approvals are not required"
     }.fetch(str) { str }
   end
 
@@ -53,10 +54,10 @@ __END__
       "status": "PASS",
       "report": {
         "default_branch_main": true,
-        "has_main_branch_protection": true,
+        "has_default_branch_protection": true,
         "requires_approving_reviews": true,
         "requires_code_owner_reviews": true,
         "administrators_require_review": true,
-        "dismisses_stale_reviews": true,
-        "team_is_admin": true,
+#         "dismisses_stale_reviews": true,
+#         "team_is_admin": true,
         "issues_enabled": true
