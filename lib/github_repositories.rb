94b76cb2e6f1625ad9c360ae5500a75bfddb373c
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-02-10 13:57:41 +0000

    Update github_repositories.rb

--- lib/github_repositories.rb
+++ lib/github_repositories.rb
@@ -2,6 +2,7 @@ class GithubRepository
   attr_reader :name, :default_branch, :report, :status, :url, :last_push, :issues_enabled
 
   FAIL = "FAIL"
+  PASS = "PASS"
 
   def initialize(hash)
     @name = hash.fetch("name")
@@ -32,6 +33,11 @@ class GithubRepository
   def fail?
     status == FAIL
   end
+  
+  def pass?
+    status == PASS
+  end
+  
 end
 
 class GithubRepositories < ItemList
@@ -43,6 +49,10 @@ class GithubRepositories < ItemList
 
   def failing
     list.filter(&:fail?)
+  end
+
+  def pass
+    list.filter(&:pass?)
   end
 end
 
