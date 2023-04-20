f7acc56d572b354eab0107cfec50c1c541a29634
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-04-20 10:48:45 +0100

    ci(tests): add pipeline to lint and test codebase
    also contains a few test additions. Using pytest rather than unittest. There's no strong reason to do this, other than to try to keep uniformity.

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -7,10 +7,28 @@ def test_config():
     assert create_app({"TESTING": True}).testing
 
 
-def test_response(client):
-    """Test the response from the app."""
+def test_home_with_no_auth(client):
+    """_summary_"""
     assert client.get("/home").status_code == 302
-    assert client.gjjet("/index").status_code == 200
-    assert client.get("/nothingthere").status_code == 404
-    assert client.get("/start").status_code == 302
+    assert client.get("/home").headers.get("location") == "/index"
 
+
+def test_start_with_no_auth(client):
+    """_summary_"""
+    assert client.get("/start").status_code == 302
+    assert client.get("/start").headers.get("location") == "/index"
+
+
+def test_login_with_no_auth(client):
+    """_summary_"""
+    assert client.get("/login").status_code == 500
+
+
+def test_callback_server_error(client):
+    """_summary_"""
+    assert client.get("/callback").status_code == 500
+
+
+def test_logout_with_no_auth(client):
+    """_summary_"""
+    assert client.get("/logout").status_code == 500
