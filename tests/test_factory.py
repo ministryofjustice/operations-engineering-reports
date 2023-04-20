007965ef57c92770aaf5e258eee7c962c8738a4b
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-04-20 15:25:02 +0100

    test(factory): remove unwanted comments

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -8,27 +8,22 @@ def test_config():
 
 
 def test_home_with_no_auth(client):
-    """_summary_"""
     assert client.get("/home").status_code == 302
     assert client.get("/home").headers.get("location") == "/index"
 
 
 def test_start_with_no_auth(client):
-    """_summary_"""
     assert client.get("/start").status_code == 302
     assert client.get("/start").headers.get("location") == "/index"
 
 
 def test_login_with_no_auth(client):
-    """_summary_"""
     assert client.get("/login").status_code == 500
 
 
 def test_callback_server_error(client):
-    """_summary_"""
     assert client.get("/callback").status_code == 500
 
 
 def test_logout_with_no_auth(client):
-    """_summary_"""
     assert client.get("/logout").status_code == 500
