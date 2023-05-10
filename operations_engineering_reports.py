bcc44025d404c7605cafd83f729712652bf14244
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-05-10 16:20:19 +0100

    revert factory changes to allo the prod version to execute (#159)
    * revert factory changes to allo the prod version to execute
    * disable pytest

--- operations_engineering_reports.py
+++ operations_engineering_reports.py
@@ -1,6 +1,5 @@
 """operations-engineering-reports"""
-from report_app import create_app
+from report_app import app
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(port=app.config.get("PORT", 4567))
