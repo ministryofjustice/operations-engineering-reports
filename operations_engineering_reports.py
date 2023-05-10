0ad53fb9b26d12ee5d211e97c823574bc7f5118c
Nick Walters <nick.walters@digital.justice.gov.uk>
2023-05-10 16:45:37 +0100

    use factory pattern with gunicorn

--- operations_engineering_reports.py
+++ operations_engineering_reports.py
@@ -1,5 +1,6 @@
 """operations-engineering-reports"""
-from report_app import app
+from report_app import create_app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(port=app.config.get("PORT", 4567))
