652697a087252ce52b13a1a97d11b4906064a4b8
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-06-16 11:46:16 +0100

    refactor(repositories): delete repositories class and replace with repository_report
    the repositories class was difficult to write unit tests, and contained a lot of obsolete code now the GitHub repository work had changed.

---
+++ report_app/main/repository_report.py
@@ -0,0 +1,63 @@
+""" The interface between the App and the AWS DynamoDB table """
+import logging
+import json
+from report_app.main.dynamodb import DynamoDB
+
+
+logger = logging.getLogger(__name__)
+
+
+class RepositoryReport:
+    """RepositoryReport represents an abstraction of the DynamoDB table
+    that stores the repository reports as a list. All public methods are
+    called by the App.
+
+    Attributes:
+        report_data (list[any): A list of repository reports
+    """
+    def __init__(self, report_data: list[any]) -> None:
+        logger.debug("Repositories.init()")
+        self._report_data = report_data
+        self.database_client = None
+
+        try:
+            self._create_db_client()
+        except Exception as exception:
+            logger.error("Failed to create database client: %s", exception)
+
+    @property
+    def _create_db_client(self):
+        dynamo_db = DynamoDB.from_context()
+        if dynamo_db is None:
+            raise Exception("Could not connect to database")
+        self.database_client = dynamo_db
+
+    @property
+    def report_data(self) -> list[str]:
+        """A list of repository reports"""
+        return self._report_data
+
+    def update_all_github_reports(self) -> None:
+        for report in self.report_data:
+            json_report = json.loads(report)
+            try:
+                self._add_report_to_db(json_report)
+            except Exception as exception:
+                raise Exception("Could not add report to database: %s", exception)
+
+    def _add_report_to_db(self, new_report: dict) -> None:
+        report_name = new_report["name"]
+        try:
+            self.database_client.add_item(report_name, new_report)
+        except Exception as exception:
+            raise exception
+
+    # def get_all_github_reports(self) -> list[dict]:
+    #     """Retrieve all reports from the database"""
+    #     logger.debug("Repositories.get_all_reports()")
+    #     try:
+    #         return self.database_client.get_all_items()
+    #     except ClientError:
+    #         raise ClientError("Could not retrieve all items from database")
+    #
+
---
+++ tests/test_repository_report.py
@@ -0,0 +1,100 @@
+import unittest
+from unittest.mock import patch
+from report_app.main.repository_report import RepositoryReport
+from moto import mock_dynamodb
+import json
+import boto3
+
+
+class TestRepositoryReport(unittest.TestCase):
+    @patch.dict('os.environ', {'DYNAMODB_TABLE_NAME': 'TEST_TABLE'})
+    @patch.dict('os.environ', {'DYNAMODB_REGION': 'eu-west-2'})
+    @patch.dict('os.environ', {'DYNAMODB_ACCESS_KEY_ID': 'FAKE'})
+    @patch.dict('os.environ', {'DYNAMODB_SECRET_ACCESS_KEY': 'FAKE'})
+    def setUp(self):
+        inpt_a = '{"name": "A"}'
+        inpt_b = '{"name": "A"}'
+        self.report_data = [
+            inpt_a,
+            inpt_b
+        ]
+
+        self.dyanmodb = mock_dynamodb()
+        self.dyanmodb.start()
+        boto3.resource('dynamodb', region_name='eu-west-2').create_table(
+            TableName='TEST_TABLE',
+            KeySchema=[
+                {
+                    'AttributeName': 'name',
+                    'KeyType': 'HASH'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'name',
+                    'AttributeType': 'S'
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
+            }
+        )
+
+        self.repository_report = RepositoryReport(self.report_data)
+
+    def test_init(self):
+        self.assertEqual(self.repository_report.report_data, self.report_data)
+        self.assertIsNotNone(self.repository_report.database_client)
+
+    def test_update_all_github_reports(self):
+        self.repository_report.update_all_github_reports()
+        test_data = self.repository_report.database_client.get_item("A")
+        self.assertEqual("A", test_data['name'])
+
+    def test_database_client_error(self):
+        self.repository_report.database_client = None
+        self.assertRaises(Exception, self.repository_report.update_all_github_reports)
+
+    @patch.object(RepositoryReport, '_add_report_to_db')
+    def test_update_all_github_reports_empty(self, mock_add_report):
+        self.repository_report._report_data = []
+        self.repository_report.update_all_github_reports()
+        mock_add_report.assert_not_called()
+
+    @patch.object(RepositoryReport, '_add_report_to_db')
+    def test_update_all_github_reports_valid_data(self, mock_add_report):
+        self.repository_report._report_data = ['{"name": "valid_report"}']
+        self.repository_report.update_all_github_reports()
+        mock_add_report.assert_called_once_with({"name": "valid_report"})
+
+    def test_update_all_github_reports_invalid_data(self):
+        self.repository_report._report_data = ['invalid_json']
+        with self.assertRaises(json.JSONDecodeError):
+            self.repository_report.update_all_github_reports()
+
+    @patch.object(RepositoryReport, '_add_report_to_db', side_effect=Exception('Failed to add repository data'))
+    def test_update_all_github_reports_client_error(self, mock_add_report):
+        self.repository_report._report_data = ['{"name": "valid_report"}']
+        with self.assertRaises(Exception):
+            self.repository_report.update_all_github_reports()
+
+    @patch.dict('os.environ', {'DYNAMODB_TABLE_NAME': 'NO_TABLE'})
+    def test_fail_to_create_db_client_no_table(self):
+        self.assertRaises(Exception, RepositoryReport(self.report_data))
+
+    def test_fail_to_start_report_with_no_client(self):
+        new_report = RepositoryReport(self.report_data)
+        new_report.database_client = None
+        self.assertRaises(Exception, new_report.update_all_github_reports)
+
+    @patch('report_app.main.dynamodb.DynamoDB.from_context', return_value=None)
+    def test_create_db_client_failure(self, mock_from_context):
+        self.assertRaises(Exception, RepositoryReport(self.report_data))
+
+    def tearDown(self):
+        self.dyanmodb.stop()
+
+
+if __name__ == '__main__':
+    unittest.main()
