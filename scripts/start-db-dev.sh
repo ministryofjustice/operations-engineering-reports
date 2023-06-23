8f7da23a72b6198b31847c7ed22a7102798c3c4f
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-06-23 17:55:46 +0100

    :fire: Remove dynamo test dir and place table in tests
    It's confusing having this directory. It would make more sense to move this to a fixtures directory and call it from there. The other content in this directory is no longer required.

--- scripts/start-db-dev.sh
+++ scripts/start-db-dev.sh
@@ -4,7 +4,8 @@ export DYNAMODB_SECRET_ACCESS_KEY=DUMMYEXAMPLEKEY
 export DYNAMODB_DEFAULT_REGION=eu-west-2
 export DYNAMODB_TABLE_NAME=cp-637250fa84046ef0
 export API_KEY=fake
+export AWES_REGION=eu-west-2
 
 docker-compose -f docker-compose.yml up -d --build
 
-aws dynamodb create-table --cli-input-json file://dynambodb_testing/create-table.json --endpoint-url http://localhost:8000 > /dev/null
+aws dynamodb create-table --cli-input-json file://tests/fixtures/create-table.json --endpoint-url http://localhost:8000 > /dev/null
