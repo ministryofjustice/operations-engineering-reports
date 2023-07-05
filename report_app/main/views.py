aa46cc09505a363cc05b171d7b9729998a92c6a8
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-07-05 14:47:11 +0100

    :adhesive_bandage: Handle server crendential mismatch
    This is a defensive measure to ensure the Auth0 credentials are appropriately set.

--- report_app/main/views.py
+++ report_app/main/views.py
@@ -28,7 +28,10 @@ def setup_auth0(setup_state):
     Args:
         setup_state (Flask): The Flask app itself
     """
-    logger.debug("setup_auth0()")
+    if __auth0_not_configured():
+        logger.warning("Auth0 not configured")
+        abort(500, "Auth0 not configured")
+
     app = setup_state.app
     OAuth(app)
     auth0 = app.extensions.get(AUTHLIB_CLIENT)
@@ -43,6 +46,12 @@ def setup_auth0(setup_state):
         + ".well-known/openid-configuration",
     )
 
+def __auth0_not_configured():
+    return (
+        os.getenv("AUTH0_CLIENT_ID") is None
+        or os.getenv("AUTH0_CLIENT_SECRET") is None
+        or os.getenv("AUTH0_DOMAIN") is None
+    )
 
 def requires_auth(function_f):
     """Redirects the web page to /index if user is not logged in
