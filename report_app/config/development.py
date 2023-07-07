b066ef41e3f7886d3002599a7c74962df99be391
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-07-07 14:50:47 +0100

    ✅ Add flask envvars to testing environment

--- report_app/config/development.py
+++ report_app/config/development.py
@@ -1,4 +1,5 @@
 """ Config values to be used during development """
+import os
 
 from os import environ
 
@@ -13,3 +14,15 @@ MAIL_FROM_EMAIL = "operations-engineering@digital.justice.gov.uk"
 PORT = 4567
 SSL_REDIRECT = False
 TESTING = True
+
+if not APP_SECRET_KEY:
+    os.environ["APP_SECRET_KEY"] = "dev"
+
+if not AUTH0_CLIENT_ID:
+    os.environ["AUTH0_CLIENT_ID"] = "dev"
+
+if not AUTH0_CLIENT_SECRET:
+    os.environ["AUTH0_CLIENT_SECRET"] = "dev"
+
+if not AUTH0_DOMAIN:
+    os.environ["AUTH0_DOMAIN"] = "dev"
