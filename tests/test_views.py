94788793ce27d05c2acaf326ba29c0c0230d40df
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-08-29 15:50:09 +0100

    ✅ Add badge and search unit tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,10 +1,13 @@
+import os
 import unittest
 from unittest.mock import MagicMock, patch
 
 from flask import current_app
 
 import report_app
-from report_app.main.views import _is_request_correct, update_github_reports
+from report_app.main.views import (_is_request_correct,
+                                   display_badge_if_compliant,
+                                   search_public_repositories)
 
 
 class TestAuth0AuthenticationView(unittest.TestCase):
@@ -71,6 +74,43 @@ class TestStandardsReportsViews(unittest.TestCase):
         self.public_landing_endpoint = "/public-github-repositories.html"
         self.private_landing_page = "/private-github-repositories.html"
 
+        self.test_public_repository = {
+            'name': 'test-public-repository',
+            'data':
+                {
+                    'is_private': False,
+                    'name': 'test-public-repository',
+                    'report':
+                        {
+                            'requires_approving_reviews': True
+                        },
+                    'default_branch': 'master',
+                    'infractions': ["infractions"],
+                    'last_push': '2023-05-24T00:45:49Z',
+                    'status': False,
+                    'url': ''
+                },
+            'stored_at': '19-06-2023 08:49:32'
+        }
+        self.test_private_repository = {
+            'name': 'test-private-repository',
+            'data':
+                {
+                    'is_private': True,
+                    'name': 'test-private-repository',
+                    'report':
+                        {
+                            'requires_approving_reviews': True
+                        },
+                    'default_branch': 'master',
+                    'infractions': [],
+                    'last_push': '2023-05-24T00:45:49Z',
+                    'status': False,
+                    'url': ''
+                },
+            'stored_at': '19-06-2023 08:49:32'
+        }
+
     @patch("os.getenv")
     def test_correct_request(self, mock_getenv):
         mock_getenv.return_value = "correct_api_key"
@@ -115,6 +155,98 @@ class TestStandardsReportsViews(unittest.TestCase):
         self.assertEqual(response.json, {"message": "GitHub reports updated"})
 
         mock_repository_report.return_value.update_all_github_reports.assert_called_once_with()
+
+    @patch('report_app.main.views.ReportDatabase')
+    def test_display_badge_if_noncompliant(self, mock_report_database):
+        repository_name = "test_public_repository"
+        mock_report_database.return_value = MagicMock()
+        mock_report_database.return_value.get_repository_report.return_value = self.test_public_repository
+
+        response = display_badge_if_compliant(repository_name)
+
+        self.assertEqual(
+            response,
+            {
+                "schemaVersion": 1,
+                "label": "MoJ Compliant",
+                "message": "FAIL",
+                "color": "d4351c",
+                "style": "for-the-badge",
+                "isError": "true",
+            },
+        )
+
+        mock_report_database.assert_called_once_with(os.getenv("DYNAMODB_TABLE_NAME"))
+
+        mock_report_database.return_value.get_repository_report.assert_called_once_with(repository_name)
+
+    @patch('report_app.main.views.ReportDatabase')
+    def test_display_badge_if_compliant(self, mock_report_database):
+        repository = self.test_public_repository
+        repository["name"] = "test_public_repository_pass"
+        repository["data"]["infractions"] = []
+        repository["data"]["status"] = True
+        mock_report_database.return_value = MagicMock()
+        mock_report_database.return_value.get_repository_report.return_value = repository
+
+        response = display_badge_if_compliant(repository["name"])
+
+        self.assertEqual(
+            response,
+            {
+                "schemaVersion": 1,
+                "label": "MoJ Compliant",
+                "message": "PASS",
+                "color": "005ea5",
+                "labelColor": "231f20",
+                "style": "for-the-badge",
+            },
+        )
+
+        mock_report_database.assert_called_once_with(os.getenv("DYNAMODB_TABLE_NAME"))
+
+        mock_report_database.return_value.response = display_badge_if_compliant(repository["name"])
+
+    @patch('report_app.main.views.ReportDatabase')
+    def test_public_github_repositories_returns_expected_behaviour(self, mock_report_database):
+        mock_report_database.return_value = MagicMock()
+        mock_report_database.return_value.get_all_public_repository_reports.return_value = [
+            self.test_public_repository
+        ]
+
+        response = self.client.get(self.public_landing_endpoint)
+
+        self.assertEqual(response.status_code, 200)
+
+        mock_report_database.assert_called_once_with(os.getenv("DYNAMODB_TABLE_NAME"))
+
+    @patch('report_app.main.views.ReportDatabase')
+    def test_private_github_repositories_returns_expected_behaviour(self, mock_report_database):
+        mock_report_database.return_value = MagicMock()
+        mock_report_database.return_value.get_all_private_repository_reports.return_value = [
+            self.test_private_repository
+        ]
+
+        response = self.client.get(self.private_landing_page)
+
+        self.assertEqual(response.status_code, 302)
+
+    @patch('report_app.main.views.ReportDatabase')
+    def test_search_public_repositories(self, mock_report_database):
+        mock_public_reports = [
+            {"name": "mock_repository_1"},
+            {"name": "mock_repository_2"},
+        ]
+
+        mock_report_database.return_value = MagicMock()
+        mock_report_database.return_value.get_all_public_repositories.return_value = mock_public_reports
+
+        response = self.client.get("/search-public-repositories", query_string={"q": "mock"})
+
+        self.assertIn("2 results found", response.data.decode("utf-8"))
+
+        mock_report_database.assert_called_once_with(os.getenv("DYNAMODB_TABLE_NAME"))
+        mock_report_database.return_value.get_all_public_repositories.assert_called_once_with()
 
 
 if __name__ == "__main__":
